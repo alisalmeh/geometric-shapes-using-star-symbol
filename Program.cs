0a95c2f720618046fddd01d38b867a7552b5bb9d
Ali Salmeh <alisalmeh18@gmail.com>
2023-08-10 16:38:04 +0330

    add initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AliSalmeh_ProjectWeek2_Refactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // right triangle
+            Console.WriteLine("*\n**\n* *\n*  *\n*   *\n******");
+
+            // filled triangle
+            Console.WriteLine("    *\n   ***\n  *****\n *******\n*********");
+
+            // triangle
+            Console.WriteLine("    *\n   * *\n  *   *\n *     *\n*********");
+
+            // dimond
+            Console.WriteLine("    *\n   * *\n  *   *\n *     *\n*       *\n *     *\n  *   *\n   * *\n    *");
+        }
+    }
+}
